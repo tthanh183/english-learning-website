eaf0ea312196e9fbf23c5d2841943d00ba9370ec
Thanh <trantanthanhttt88@gmail.com>
2023-05-07 09:55:14 +0700

    char.js temporary

--- PBL3-web-logic/src/main/java/vn/pbl/controller/admin/StatisticController.java
+++ PBL3-web-logic/src/main/java/vn/pbl/controller/admin/StatisticController.java
@@ -1,7 +1,5 @@
 package vn.pbl.controller.admin;
 
-import org.apache.commons.lang.StringUtils;
-import vn.pbl.command.ListenGuidelineCommand;
 import vn.pbl.core.web.utils.SingletonServiceUtil;
 
 import javax.servlet.RequestDispatcher;
@@ -11,9 +9,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
-
 @WebServlet("/admin-user-statistic.html")
 public class StatisticController extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
@@ -25,24 +20,24 @@ public class StatisticController extends HttpServlet {
         Long male = SingletonServiceUtil.getUserServiceInstance().maleCount(null,null,null,null,null);
 //        Object[] objects2 = SingletonServiceUtil.getUserServiceInstance().findByProperty(femaleMap,null,null,null,null);
 //        Long female = (Long)objects2[0];
-        Long female = SingletonServiceUtil.getUserServiceInstance().femaleCount(null,null,null,null,null);;
+        Long female = SingletonServiceUtil.getUserServiceInstance().femaleCount(null,null,null,null,null);
         request.setAttribute("maleCount", male);
         request.setAttribute("femaleCount",female );
         RequestDispatcher rd = request.getRequestDispatcher("/views/admin/statistics/userstatistic.jsp");
         rd.forward(request, response);
     }
-    private Map<String,Object> buildMapPropertiesMale() {
-        Map<String, Object> properties = new HashMap<String, Object>();
-        String male = null;
-        properties.put("gender",male);
-        return properties;
-    }
-    private Map<String,Object> buildMapPropertiesFeMale() {
-        Map<String, Object> properties = new HashMap<String, Object>();
-        String female = null;
-        properties.put("gender",female);
-        return properties;
-    }
+//    private Map<String,Object> buildMapPropertiesMale() {
+//        Map<String, Object> properties = new HashMap<String, Object>();
+//        String male = null;
+//        properties.put("gender",male);
+//        return properties;
+//    }
+//    private Map<String,Object> buildMapPropertiesFeMale() {
+//        Map<String, Object> properties = new HashMap<String, Object>();
+//        String female = null;
+//        properties.put("gender",female);
+//        return properties;
+//    }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
